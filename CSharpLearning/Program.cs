685d0739d1359956a2716d969abb39c6d6334129
hossein abdizadeh <79523696+h-abdizadeh@users.noreply.github.com>
2022-12-28 12:37:16 +0330

    rename practice

--- CSharpLearning/Program.cs
+++ CSharpLearning/Program.cs
@@ -1,12 +1,12 @@
 ﻿//c-sharp learning - dotNet 6                   //آموزش سی شارپ - دات نت 6
 
 ///////////////////////////////////////////////////////////////////////////////////////
-//practice 1 _ data types and variables          //تمرین ا _ انواع داده و تعریف متغیر
+//practice 1 _ data types and variables         //تمرین ا _ انواع داده و تعریف متغیر
 //practice 2 _ Write and WriteLine (output)     //تمرین 2 _       نمایش خروجی برنامه   
 //practice 3 _ ReadLine and ReadKey (input)     //تمرین 3 _             دریافت ورودی   
 //practice 4 _ convert data types               //تمرین 4 _         تبدیل انواع داده   
-//program 1 _ simple calculations               //برنامه 1 _             محاسبات ساده   
-
+//program 1 _ simple calculations               //برنامه 1 _             محاسبات ساده      
+//practice 5 _ if else                          //تمرین 5 _             دستورات شرطی
 
 //to be continue...                             //ادامه دارد...
 
@@ -22,3 +22,5 @@
 //right-click on practice or program
 //set as startup project
 //press F5
+
+Console.Write("https://github.com/h-abdizadeh/CSharpLearning");
